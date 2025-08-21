00db87695f7e0ed3785b119ef80623c5d42bc5fb
SiddhantKholkar1999 <siddhantkholkar1999@gmail.com>
2025-08-21 16:57:00 +0530

    My Bakery Project

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const router = jsonServer.router('db.json');
 
 // Set default middlewares (CORS, JSON)
 app.use(cors());
+app.use(cors({
+    origin: ["http://127.0.0.1:5500", "http://localhost:3000", "*"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(jsonServer.defaults());
 
 // Bind the router db to the app
